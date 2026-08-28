f7eaf8f19f0ec63e727796be11ff32c30ecddbca
purpurcof <122213198+purpurcof@users.noreply.github.com>
2026-08-28 17:20:56 +0500

    Migration to a new repository x2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,8 +4,7 @@ pluginManagement {
     repositories {
         gradlePluginPortal()
         mavenCentral()
-        maven("https://registry.whereareiam.me/release")
-        maven("https://registry.whereareiam.me/development")
+        maven("https://registry.whereareiam.me/maven/packages")
         maven("https://repo.codemc.io/repository/maven-releases/")
         maven("https://repo.codemc.io/repository/maven-snapshots/")
     }
@@ -15,8 +14,7 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         mavenCentral()
-        maven("https://registry.whereareiam.me/release")
-        maven("https://registry.whereareiam.me/development")
+        maven("https://registry.whereareiam.me/maven/packages")
         maven("https://repo.papermc.io/repository/maven-public/")
         maven("https://oss.sonatype.org/content/repositories/snapshots/")
         maven("https://repo.codemc.io/repository/maven-releases/")
